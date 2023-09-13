3fabe65a8a72d264b358249de5feb219dd50555d
Your Name <you@example.com>
2023-09-13 11:51:38 +0530

    sa

--- js/index.js
+++ js/index.js
@@ -28,27 +28,6 @@ $(document).ready(function () {
 
 
 
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 30,
-    autoplay: true,
-    responsiveClass: true,
-    responsive: {
-        0: {
-            items: 1,
-            nav: false
-        },
-        600: {
-            items: 2,
-            nav: false
-        },
-        1000: {
-            items: 3,
-            nav: false,
-            loop: true
-        }
-    }
-})
 
 
 
